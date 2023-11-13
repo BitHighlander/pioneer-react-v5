60e9691833f956616c87458015dd43f87e2c879c
bithighlander <bithighlander@gmail.com>
2023-11-12 20:41:54 -0700

    works!

---
+++ src/lib/components/button/index.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@chakra-ui/react';
+
+const HelloWorldButton = () => {
+  return <Button onClick={() => alert('Hello World!')}>Hello World</Button>;
+};
+
+export default HelloWorldButton;
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import million from 'million/compiler';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,4 +20,14 @@ export default defineConfig({
   server: {
     open: true,
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"), // Set the entry file of your library
+      name: "PioneerLib", // Set the name of your library
+      fileName: (format) => `my-library.${format}.js` // Set the fileName of your library
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', '@chakra-ui/react', '@emotion/react'],
+    }
+  }
 });
